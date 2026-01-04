17d528fcac72de3dd22bec2d539d0257233b8f5e
Shinichi Taira <t.shinichi@gmail.com>
2026-01-04 18:30:02 +0900

    Initial commit - Matsuyama Adult Trip Site

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const slides = document.querySelectorAll('.slide');
+    let currentSlide = 0;
+
+    function nextSlide() {
+        slides[currentSlide].classList.remove('active');
+        currentSlide = (currentSlide + 1) % slides.length;
+        slides[currentSlide].classList.add('active');
+    }
+
+    // Change slide every 5 seconds
+    setInterval(nextSlide, 5000);
+});
